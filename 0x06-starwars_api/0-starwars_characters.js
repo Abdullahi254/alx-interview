69dadc30f00c579045b6fcb1478f80846937d7bc
Abdullahi254 <abdush504@gmail.com>
2024-02-14 13:49:07 +0300

    consuming api

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,10 @@
+const request = require('request');
+
+request(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`, (error, response, body) => {
+    const charactersUrl = JSON.parse(body).characters;
+    charactersUrl.forEach(url => {
+        request(url, (error, response, body) => {
+            console.log(JSON.parse(body).name);
+        });
+    });
+});
